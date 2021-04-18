c446a41d6a0908c9a02511eaea65a1bd239dec53
RAHUL SHARMA <73328975+RAHULSHARMA63@users.noreply.github.com>
2021-04-18 17:41:16 +0530

    To find the area of rectangle.

---
+++ assign.1.1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+   int length, breadth, area;
+  
+   cout << "\n enter the length of rectangle : ";
+   cin>>length;
+
+   cout << "\n enter the breadth of rectangle : ";
+   cin>>breadth;
+
+   area = length * breadth;
+   cout << "\n area of Rectangle : " << area;
+ 
+   return 0;
+}
